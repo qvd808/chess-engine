0f92f05152b15be25d5c5d2947f0899fcdc3c5b1
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-11-04 21:19:35 -0700

    Lan dau tien day code len mang

---
+++ display.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Display():
+    def __init__(self) -> None:
+        pass
+
+    def runDisplay():
+        pass
---
+++ main.py
@@ -0,0 +1,14 @@
+from display import Display
+
+class MainGame ():
+    def __init__(self) -> None:
+        self.display = Display()
+    
+    def run(self):
+        self.display.runDisplay()
+
+
+
+if __name__ == "__main__":
+    game = MainGame()
+    game.run()

442cf3dcf7c7d10b41413456f3258360b730625c
qvd808 <dqvinh808@gmail.com>
2022-11-11 15:09:11 -0800

    Adding more error correction for Piece

--- component/piece/Piece.py
+++ component/piece/Piece.py
@@ -1,10 +1,13 @@
 class Piece():
-    def __init__(self, position) -> None:
+    def __init__(self, position, color) -> None:
 
         self.RaisePositionError(position)
+        self.RaiseColorError(color)
 
         self.col = position[0]
         self.row = position[1]
+
+        self.color = color
     
     def move(self, new_position) -> bool:
 
@@ -20,6 +23,9 @@ class Piece():
 
 
     def RaisePositionError(self, position):
+        if not isinstance(position, str):
+            raise ValueError("Value must be a string")
+
         if len(position) != 2:
             raise ValueError("Length of position should be two")
 
@@ -28,6 +34,13 @@ class Piece():
 
         if  not 'A' <= position[0] <= 'H':
             raise ValueError("Invalid input for the column")
+
+    def RaiseColorError(self, color):
+        if not isinstance(color, str):
+            raise ValueError("Value must be a string")
+        
+        if color != 'white' and color != 'black':
+            raise ValueError("Value must be a string that is 'white' or 'black'")
 
     def getPosition(self) -> str:
         return self.col + self.row
--- component/piece/Piece.test.py
+++ component/piece/Piece.test.py
@@ -6,8 +6,8 @@ from Piece import Piece
 class PieceUnitTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.piece1 = Piece('H5')
-        self.piece2 = Piece('A8')
+        self.piece1 = Piece('H5', 'white')
+        self.piece2 = Piece('A8', 'black')
 
 
     def test_Get_Position(self):
@@ -19,17 +19,29 @@ class PieceUnitTest(unittest.TestCase):
 
     def test_RaiseErrorIfLengthNotTwo(self):
         with self.assertRaises(ValueError):
-            errorPiece = Piece('H58')
+            errorPiece = Piece('H58', 'white')
 
         with self.assertRaises(ValueError):
-            errorPiece = Piece('8')
+            errorPiece = Piece('8', 'black')
 
     def test_RaiseErrorIfValueIsNotValid(self):
         with self.assertRaises(ValueError):
-            errorPiece = Piece('g5')
+            errorPiece = Piece('g5', 'white')
 
         with self.assertRaises(ValueError):
-            errorPiece = Piece('8g')
+            errorPiece = Piece('8g', 'black')
+
+        with self.assertRaises(ValueError):
+            errorPiece = Piece('G5', 'red')
+
+        with self.assertRaises(ValueError):
+            errorPiece = Piece('C4', 'blue')
+        
+        with self.assertRaises(ValueError):
+            errorPiece = Piece(32, 6)
+
+        with self.assertRaises(ValueError):
+            errorPiece = Piece(True, [])
 
     
     def test_Move_Piece(self):
@@ -59,6 +71,10 @@ class PieceUnitTest(unittest.TestCase):
     def test_Get_Position(self):
         self.assertEqual(self.piece1.getPosition(), 'H5')
         self.assertEqual(self.piece2.getPosition(), 'A8')
+
+    def test_getColor(self):
+        self.assertEqual(self.piece1.color, 'white')
+        self.assertEqual(self.piece2.color, 'black')
 
 
 if __name__ == "__main__":

5a2fcfe88b1381c316f91c05a4e915bbb50d9c8c
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-11-13 11:25:47 -0800

    Added drawboard method for the display

--- utils/display.py
+++ utils/display.py
@@ -3,28 +3,14 @@ import pygame
 # Display should only worries about how to display something on the screen, it should not care about render or other method that it need to worries about
 # Render should not worried about calculating the coordinate and should leave it to another classes
 
-class CoordinateCalculator:
-    def __init__(self) -> None:
-        pass
-        
-
-class Render():
-    def __init__(self) -> None:
-        self.draw = pygame.draw
-        self.WHITE = (255, 255, 255)
-
-    def draw_board(self, surface):
-        self.draw.rect(surface, self.WHITE, (200, 200, 100, 100), 1)
-
 class Display():
     def __init__(self, width = 800, height = 600) -> None:
         pygame.init()
 
         self.width = width
         self.height = height
-        self.surface = None
 
-        self.render = Render()
+        self.render = Render(self)
 
     def turnOffDisplayOnExitButton(self, exit_game):
         for event in pygame.event.get():
@@ -32,7 +18,7 @@ class Display():
                 exit_game()
 
     def draw_board(self):
-        self.render.draw_board(self.surface)
+        self.render.draw_board(self)
 
     def runDisplayUpdate(self):
         pygame.display.update()
@@ -40,3 +26,42 @@ class Display():
     def runDisplay(self):
         pygame.display.init()
         self.surface = pygame.display.set_mode(size = (self.width, self.height))
+
+
+class Render():
+    def __init__(self, display:Display) -> None:
+        self.draw = pygame.draw
+        self.WHITE = (255, 255, 255)
+        self.BROWN = (165, 42, 42)
+
+        self.__coorCalculator = CoordinateCalculator(display)
+
+
+    def draw_board(self, display: Display):
+
+        xCor, yCor, width = self.__coorCalculator.boardCordinate(display)
+        surface = display.surface
+
+        self.draw.rect(surface, self.BROWN, (xCor, yCor, width, width), 1)
+
+        for i in range(8):
+            for j in range(4):
+                if i % 2 == 0:
+                    self.draw.rect(surface, self.WHITE, (xCor + width//4 * j, yCor + width//8 * i, width//8, width//8))
+                else:
+                    self.draw.rect(surface, self.WHITE, ((xCor + width//8) + width//4 * j, yCor + width//8 * i, width//8, width//8))
+        
+
+class CoordinateCalculator:
+    def __init__(self, display:Display) -> None:
+        self.board_area_percentage = 60
+
+    def boardCordinate(self, display: Display):
+
+        width = int(self.board_area_percentage * display.width // 100)
+        x = int(display.width // 2 - width // 2)
+        y = int(display.height // 2 - width // 2)
+
+        return (x, y, width)
+    
+
